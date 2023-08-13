1c706cb16c1da42767b7e14056a42017cadc2ab4
vi-sha-kha <vishakha.ch.5281@gmail.com>
2023-08-13 10:39:56 +0545

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,197 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div class="container">
+          <div class="footer-top">
+            <div class="row">
+              <div class="col-md-6 col-lg-3 about-footer mb-10">
+                <img
+                  src="https://siteadmin.nabilbank.com/assets/backend/uploads/logo.png"
+                  alt="Nabil Bank"
+                  class="img-fluid"
+                  width="220px"
+                />
+                <div class="footer__pageLinks">
+                  <div>
+                    <h5>Customer Support</h5>
+                    <ul class="list-unstyled">
+                      <li>
+                        <img
+                          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAMBAMAAACgrpHpAAAAKlBMVEWWlpZHcEyVlZWWlpadnZ2VlZWVlZWXl5eVlZWVlZWWlpaVlZWVlZWVlZVPm36+AAAADnRSTlNmAP8hB1RgON8TRMSkm8K3SkgAAABOSURBVAjXYxBoUlJS6pnIkKQEAhvhtBRIXNeRIdkaSKcEMzCUHFJqn8rMwMDl0DM5AUgzRIoUMIBohgQGIB0AYjFwMhiA6YUMwiCBdEcAqHQQdntM260AAAAASUVORK5CYII="
+                          class="img-fluid mr-2"
+                          alt=""
+                        />
+                        <a href="mailto:customercare@nabilbank.com">
+                          customercare@nabilbank.com
+                        </a>
+                      </li>
+                      <li>
+                        <img
+                          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMBAMAAACkW0HUAAAALVBMVEVHcEyYmJiVlZWWlpaUlJSWlpaVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZX/ezgkAAAADnRSTlMACFoxF0TzvW4p1pnjgsA+ap4AAABWSURBVAjXY2BtYBAKYWDQe83AOMmAIe+dgZChCsO6dxNYlKcy3HvOwOAkxCDzzIGBkZGBNa+QAQT0njuYAimOd+nvNgJptXfvXgAp5nXvnoJkJZYpAACTwBa2SWxQ0AAAAABJRU5ErkJggg=="
+                          class="img-fluid mr-2"
+                          alt=""
+                        />
+                        <a href="tel:+977 1 5970015">+977 1 5970015</a>
+                      </li>
+                      <li>SWIFT: NARBNPKA</li>
+                    </ul>
+                  </div>
+                </div>
+                <ul>
+                  <li>
+                    <a href="tel:(010) 1234 4321">
+                      <i class="fas fa-phone fa-flip-horizontal"></i>(010) 1234
+                      4321
+                    </a>
+                  </li>
+                  <li>
+                    <i class="fas fa-map-marker-alt"></i>
+                    1 / 105 Bay Lights,
+                    <br />
+                    Lorem Ipsum,
+                    <br />
+                    LIC 3201
+                  </li>
+                </ul>
+                <a href="" class="btn red-btn">
+                  Book Now
+                </a>
+              </div>
+              <div class="col-md-6 col-lg-2 page-more-info">
+                <div class="footer-title">
+                  <h4>Page links</h4>
+                </div>
+                <ul>
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Testimonial</a>
+                  </li>
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="col-md-6 col-lg-3 page-more-info">
+                <div class="footer-title">
+                  <h4>More Info</h4>
+                </div>
+                <ul>
+                  <li>
+                    <a href="#">Lorem ipsum</a>
+                  </li>
+                  <li>
+                    <a href="#">Dolor sit amet</a>
+                  </li>
+                  <li>
+                    <a href="#">Consectetur Adipisicing </a>
+                  </li>
+                  <li>
+                    <a href="#">Ed do eiusmod tempor incididunt</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-md-6 col-lg-4 open-hours">
+                <div class="footer-title">
+                  <h4>Open hours</h4>
+                  <ul class="footer-social">
+                    <li>
+                      <a href="" target="_blank">
+                        <i class="fab fa-facebook-f"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="" target="_blank">
+                        <i class="fab fa-instagram"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="" target="_blank">
+                        <i class="fab fa-linkedin-in"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <table class="table">
+                  <tbody>
+                    <tr>
+                      <td>
+                        <i class="far fa-clock"></i>Monday Thursday
+                      </td>
+                      <td>9:00am - 5:00pm</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <i class="far fa-clock"></i>Friday
+                      </td>
+                      <td>9:00am - 4:00pm</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <i class="far fa-clock"></i>Sturday
+                      </td>
+                      <td>9:00am - 1:30pm</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <i class="far fa-clock"></i>Sunday
+                      </td>
+                      <td>9:30am - 12:00pm</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <hr />
+                <div class="footer-logo">
+                  <table>
+                    <tbody>
+                      <tr />
+                      <td>
+                        <img src="https://i.ibb.co/vxc577d/dummy-logo3.jpg" />
+                      </td>
+                      <td>
+                        <img src="https://i.ibb.co/vxc577d/dummy-logo3.jpg" />
+                      </td>
+                      <td>
+                        <img src="https://i.ibb.co/vxc577d/dummy-logo3.jpg" />
+                      </td>
+                      <td>
+                        <img src="https://i.ibb.co/vxc577d/dummy-logo3.jpg" />
+                      </td>
+                      <td>
+                        <img src="https://i.ibb.co/vxc577d/dummy-logo3.jpg" />
+                      </td>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div class="footer-bottom">
+            <div class="row">
+              <div class="col-sm-4">
+                <a href="">Privacy policy</a>
+              </div>
+              <div class="col-sm-8">
+                <p>Lorem ipsum dolor sit amet @ 2019 All rights reserved</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
