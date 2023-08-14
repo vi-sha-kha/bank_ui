fc7509baa1db49372ea48b338142dbf7186ea0c6
vi-sha-kha <vishakha.ch.5281@gmail.com>
2023-08-14 12:43:25 +0545

    added props

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-const Cards = () => {
+const Cards = ({ img, title }) => {
   return (
     <>
       <div
-        class="card mb-3 shadow-lg p-3 mb-5 bg-white rounded"
-        style={{ maxWidth: "5px;" }}
+        class="card mb-3 shadow-lg p-3 mb-5 bg-white rounded w-10 h-10"
+        // style={{ maxWidth: "5px;", minHeight: "5px;" }}
       >
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="..." class="img-fluid rounded-start" alt="...TO ADD" />
+            <img src={img} class="img-fluid rounded-start" alt="...TO ADD" />
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">{title}</h5>
             </div>
           </div>
         </div>
--- src/components/CardsScrollable.js
+++ src/components/CardsScrollable.js
@@ -5,14 +5,44 @@ import Navbar from "react-bootstrap/Navbar";
 import { NavHashLink } from "react-router-hash-link";
 import { useLocation } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+
 const CardsScrollable = () => {
   const { hash } = useLocation();
   const isActive = (iHash) => hash === iHash;
+
+  const cardData = [
+    {
+      path: "/individual/interest-rate",
+      img: "https://siteadmin.nabilbank.com/assets/backend/uploads/Banner Menu/interest.png",
+      title: "Interest Rate",
+    },
+    {
+      path: "/individual/currency",
+      img: "https://siteadmin.nabilbank.com/assets/backend/uploads/Banner Menu/Exchange rates.png",
+      title: "Forex Rates",
+    },
+    {
+      path: "/individual/calculator",
+      img: "https://siteadmin.nabilbank.com/assets/backend/uploads/Banner Menu/budget.png",
+      title: "Calculator",
+    },
+    {
+      path: "/individual/grievance",
+      img: "https://siteadmin.nabilbank.com/assets/backend/uploads/Banner Menu/Grievance.png",
+      title: "Grievance",
+    },
+    {
+      path: "/individual/calendar",
+      img: "https://siteadmin.nabilbank.com/assets/backend/uploads/Banner Menu/WhatsApp Image 2023-04-12 at 15.36.11.jpg",
+      title: "Calendar",
+    },
+    // Add more card data as needed
+  ];
   return (
     <>
       <div class="d-flex flex-row flex-nowrap overflow-auto row-horizon">
         {/* <div className="row row-horizon"> */}
-        <div className="col-xs-6">
+        {/* <div className="col-xs-6">
           <Nav.Link
             activeClassName="active-link" // <-- define active classname
             as={HashLink} // <-- render NavHashLink component
@@ -106,100 +136,31 @@ const CardsScrollable = () => {
           >
             <Cards />
           </Nav.Link>
-        </div>
+        </div> */}
+
+        {cardData.map((card, index) => (
+          <div key={index} className="col-xs-6">
+            <Nav.Link
+              activeClassName="active-link"
+              as={NavHashLink}
+              className="style-navlink"
+              smooth
+              to={card.path}
+              style={
+                isActive(card.path)
+                  ? {
+                      fontWeight: "bold",
+                      backgroundColor: "rgb(213, 41, 51)",
+                    }
+                  : {}
+              }
+            >
+              <Cards img={card.img} title={card.title} />
+            </Nav.Link>
+          </div>
+        ))}
         {/* </div> */}
       </div>
-
-      {/* <div class="container-fluid">
-        <div class="row">
-          <div class="icon-tab col-xs-2 col-xs-offset-3 active">
-            <span class="glyphicon glyphicon-music"></span>
-            <span class="icon-label">Musics</span>
-          </div>
-          <div class="icon-tab col-xs-2 col-sm-2 ">
-            <span class="glyphicon glyphicon-camera"></span>
-            <span class="icon-label">Pictures</span>
-          </div>
-          <div class="icon-tab col-xs-2 col-sm-2">
-            <span class="glyphicon glyphicon-facetime-video"></span>
-            <span class="icon-label">Movies</span>
-          </div>
-        </div>
-      </div> */}
-      {/* 
-      <Nav className="justify-content-start flex-grow-1 pe-3">
-        <Nav.Link
-          activeClassName="active-link" // <-- define active classname
-          as={HashLink} // <-- render NavHashLink component
-          className="style-navlink"
-          smooth
-          to="/individual/interest-rate"
-          style={
-            isActive("/individual/interest-rate")
-              ? {
-                  fontWeight: "bold",
-                  backgroundColor: "rgb(213, 41, 51)",
-                }
-              : {}
-          }
-        >
-          Interest Rate
-        </Nav.Link>
-        <Nav.Link
-          activeClassName="active-link" // <-- define active classname
-          as={HashLink} // <-- render NavHashLink component
-          className="style-navlink"
-          smooth
-          to="/individual/currency"
-          style={
-            isActive("/individual/currency")
-              ? {
-                  fontWeight: "bold",
-                  backgroundColor: "rgb(213, 41, 51)",
-                }
-              : {}
-          }
-        >
-          Forex Rates
-        </Nav.Link>
-        <Nav.Link
-          activeClassName="active-link" // <-- define active classname
-          as={HashLink} // <-- render NavHashLink component
-          className="style-navlink"
-          smooth
-          to="/individual/calendar"
-          style={
-            isActive("/individual/calculator")
-              ? {
-                  fontWeight: "bold",
-                  backgroundColor: "rgb(213, 41, 51)",
-                }
-              : {}
-          }
-        >
-          Calculator
-        </Nav.Link>
-        <Nav.Link
-          activeClassName="active-link" // <-- define active classname
-          as={HashLink} // <-- render NavHashLink component
-          className="style-navlink"
-          smooth
-          to="/individual/grievance-handling"
-          style={
-            isActive("/individual/calculator")
-              ? {
-                  fontWeight: "bold",
-                  backgroundColor: "rgb(213, 41, 51)",
-                }
-              : {}
-          }
-        >
-          Calculator
-        </Nav.Link>
-        <Nav.Link href="https://siteadmin.nabilbank.com/assets/backend/uploads/Calendar/Nabil-Calendar-2080.pdf">
-          Calendar
-        </Nav.Link>
-      </Nav> */}
     </>
   );
 };
