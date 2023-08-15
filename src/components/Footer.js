084d501cbb0c244766739d775eb505570ea0f407
vi-sha-kha <vishakha.ch.5281@gmail.com>
2023-08-15 07:55:37 +0545

    modified footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -42,18 +42,178 @@ const Footer = () => {
                 </div>
                 <ul>
                   <li>
-                    <a href="tel:(010) 1234 4321">
-                      <i class="fas fa-phone fa-flip-horizontal"></i>(010) 1234
-                      4321
-                    </a>
+                    <img
+                      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADIAQMAAACXljzdAAAABlBMVEUAAAD///+l2Z/dAAAB90lEQVRYR+XSMaodOhADUIG2NaCtC7QtgX5x3w9Jkc63isB44FSWB/tb8A+JARrsqbLKLAb4XLJlMWduBVNly3sxY/Vo+ZiyivkdSU/mMqsqvydbVQBQgS/Jliq+41JZ229dvxMDzJ/5bXfeya+Uy8yC2/ZcDCVLUoCFtsrMc/l/WpKquGOVfUHMrUrVk2nG99PoSymgVEllbmahKu9FhWaZM1fcHVc9l1VL1lN64HpHc8/FLEAffbhTrOL0XpbNd1rvcJAhH/RcYgBHq7R8R6unPBefeky5n2Nt1jckVlkwSUrjPi99K/HJKjfLTHpQyudiGlx6B7DcYjB7LgVnKGVmGVxm5b0osXpckhTHxOBz2bLE9HFZqmTpeykNFjgA0KwYuC8IWFW+g8ptq5Z8QYCDqjJlyq3a3otSzaf5ACabwbyXUz7zNh9T4L4gq3qQuR5UGQeaz8UqsxVK71OqVj2XFKceDfXoOzBbnsssHyvf4RRrSU/vJVZ6WrLNtKpl72XxHZP4FOPAGHwuBdcD6MNxZXx37yVleifT4KdbcHsu22ZtPS7m5/5syFMxwEKxEnPGMcmeS7aUyZLFTJUZei9mqt7JYFUWwPErkvXAWevRWj5/+lx8LBhzmQ8q8162pKefWq0C3HsxwCSFypk+Zcmey9/y78h/9KkebcIJnBwAAAAASUVORK5CYII="
+                      class="img-fluid"
+                      width="80px"
+                      alt=""
+                    />
                   </li>
                   <li>
-                    <i class="fas fa-map-marker-alt"></i>
-                    1 / 105 Bay Lights,
-                    <br />
-                    Lorem Ipsum,
-                    <br />
-                    LIC 3201
+                    <div>
+                      <h5>Follow us</h5>
+                      <ul class="list-unstyled d-flex social-link-lists">
+                        <li>
+                          <a
+                            href="https://www.facebook.com/nabilbanklimited"
+                            class="facebook-icon"
+                            target="_blank"
+                          >
+                            <svg
+                              stroke="currentColor"
+                              fill="currentColor"
+                              stroke-width="0"
+                              viewBox="0 0 24 24"
+                              height="1em"
+                              width="1em"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <g>
+                                <path fill="none" d="M0 0h24v24H0z"></path>
+                                <path d="M14 13.5h2.5l1-4H14v-2c0-1.03 0-2 2-2h1.5V2.14c-.326-.043-1.557-.14-2.857-.14C11.928 2 10 3.657 10 6.7v2.8H7v4h3V22h4v-8.5z"></path>
+                              </g>
+                            </svg>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.instagram.com/nabilbank/"
+                            class="instagram-icon"
+                            target="_blank"
+                          >
+                            <svg
+                              stroke="currentColor"
+                              fill="currentColor"
+                              stroke-width="0"
+                              viewBox="0 0 24 24"
+                              height="1em"
+                              width="1em"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <g>
+                                <path fill="none" d="M0 0h24v24H0z"></path>
+                                <path
+                                  fill-rule="nonzero"
+                                  d="M12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm0-2a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm6.5-.25a1.25 1.25 0 0 1-2.5 0 1.25 1.25 0 0 1 2.5 0zM12 4c-2.474 0-2.878.007-4.029.058-.784.037-1.31.142-1.798.332-.434.168-.747.369-1.08.703a2.89 2.89 0 0 0-.704 1.08c-.19.49-.295 1.015-.331 1.798C4.006 9.075 4 9.461 4 12c0 2.474.007 2.878.058 4.029.037.783.142 1.31.331 1.797.17.435.37.748.702 1.08.337.336.65.537 1.08.703.494.191 1.02.297 1.8.333C9.075 19.994 9.461 20 12 20c2.474 0 2.878-.007 4.029-.058.782-.037 1.309-.142 1.797-.331.433-.169.748-.37 1.08-.702.337-.337.538-.65.704-1.08.19-.493.296-1.02.332-1.8.052-1.104.058-1.49.058-4.029 0-2.474-.007-2.878-.058-4.029-.037-.782-.142-1.31-.332-1.798a2.911 2.911 0 0 0-.703-1.08 2.884 2.884 0 0 0-1.08-.704c-.49-.19-1.016-.295-1.798-.331C14.925 4.006 14.539 4 12 4zm0-2c2.717 0 3.056.01 4.122.06 1.065.05 1.79.217 2.428.465.66.254 1.216.598 1.772 1.153a4.908 4.908 0 0 1 1.153 1.772c.247.637.415 1.363.465 2.428.047 1.066.06 1.405.06 4.122 0 2.717-.01 3.056-.06 4.122-.05 1.065-.218 1.79-.465 2.428a4.883 4.883 0 0 1-1.153 1.772 4.915 4.915 0 0 1-1.772 1.153c-.637.247-1.363.415-2.428.465-1.066.047-1.405.06-4.122.06-2.717 0-3.056-.01-4.122-.06-1.065-.05-1.79-.218-2.428-.465a4.89 4.89 0 0 1-1.772-1.153 4.904 4.904 0 0 1-1.153-1.772c-.248-.637-.415-1.363-.465-2.428C2.013 15.056 2 14.717 2 12c0-2.717.01-3.056.06-4.122.05-1.066.217-1.79.465-2.428a4.88 4.88 0 0 1 1.153-1.772A4.897 4.897 0 0 1 5.45 2.525c.638-.248 1.362-.415 2.428-.465C8.944 2.013 9.283 2 12 2z"
+                                ></path>
+                              </g>
+                            </svg>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://twitter.com/nabilbank"
+                            class="twitter-icon"
+                            target="_blank"
+                          >
+                            <svg
+                              stroke="currentColor"
+                              fill="currentColor"
+                              stroke-width="0"
+                              viewBox="0 0 24 24"
+                              height="1em"
+                              width="1em"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <g>
+                                <path fill="none" d="M0 0h24v24H0z"></path>
+                                <path d="M22.162 5.656a8.384 8.384 0 0 1-2.402.658A4.196 4.196 0 0 0 21.6 4c-.82.488-1.719.83-2.656 1.015a4.182 4.182 0 0 0-7.126 3.814 11.874 11.874 0 0 1-8.62-4.37 4.168 4.168 0 0 0-.566 2.103c0 1.45.738 2.731 1.86 3.481a4.168 4.168 0 0 1-1.894-.523v.052a4.185 4.185 0 0 0 3.355 4.101 4.21 4.21 0 0 1-1.89.072A4.185 4.185 0 0 0 7.97 16.65a8.394 8.394 0 0 1-6.191 1.732 11.83 11.83 0 0 0 6.41 1.88c7.693 0 11.9-6.373 11.9-11.9 0-.18-.005-.362-.013-.54a8.496 8.496 0 0 0 2.087-2.165z"></path>
+                              </g>
+                            </svg>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.linkedin.com/company/nabil-bank/"
+                            target="_blank"
+                            class="linkedin-icon"
+                          >
+                            <svg
+                              stroke="currentColor"
+                              fill="currentColor"
+                              stroke-width="0"
+                              viewBox="0 0 24 24"
+                              height="1em"
+                              width="1em"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <g>
+                                <path fill="none" d="M0 0h24v24H0z"></path>
+                                <path
+                                  fill-rule="nonzero"
+                                  d="M6.94 5a2 2 0 1 1-4-.002 2 2 0 0 1 4 .002zM7 8.48H3V21h4V8.48zm6.32 0H9.34V21h3.94v-6.57c0-3.66 4.77-4 4.77 0V21H22v-7.93c0-6.17-7.06-5.94-8.72-2.91l.04-1.68z"
+                                ></path>
+                              </g>
+                            </svg>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="viber://chat?number=+9779802002095"
+                            target="_blank"
+                            class="viber-icon"
+                          >
+                            <svg
+                              stroke="currentColor"
+                              fill="currentColor"
+                              stroke-width="0"
+                              role="img"
+                              viewBox="0 0 24 24"
+                              height="1em"
+                              width="1em"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <title></title>
+                              <path d="M11.398.002C9.473.028 5.331.344 3.014 2.467 1.294 4.177.693 6.698.623 9.82c-.06 3.11-.13 8.95 5.5 10.541v2.42s-.038.97.602 1.17c.79.25 1.24-.499 1.99-1.299l1.4-1.58c3.85.32 6.8-.419 7.14-.529.78-.25 5.181-.811 5.901-6.652.74-6.031-.36-9.831-2.34-11.551l-.01-.002c-.6-.55-3-2.3-8.37-2.32 0 0-.396-.025-1.038-.016zm.067 1.697c.545-.003.88.02.88.02 4.54.01 6.711 1.38 7.221 1.84 1.67 1.429 2.528 4.856 1.9 9.892-.6 4.88-4.17 5.19-4.83 5.4-.28.09-2.88.73-6.152.52 0 0-2.439 2.941-3.199 3.701-.12.13-.26.17-.35.15-.13-.03-.17-.19-.16-.41l.02-4.019c-4.771-1.32-4.491-6.302-4.441-8.902.06-2.6.55-4.732 2-6.172 1.957-1.77 5.475-2.01 7.11-2.02zm.36 2.6a.299.299 0 0 0-.3.299.3.3 0 0 0 .3.3 5.631 5.631 0 0 1 4.03 1.59c1.09 1.06 1.621 2.48 1.641 4.34a.3.3 0 0 0 .3.3v-.009a.3.3 0 0 0 .3-.3 6.451 6.451 0 0 0-1.81-4.76c-1.19-1.16-2.692-1.76-4.462-1.76zm-3.954.69a.955.955 0 0 0-.615.12h-.012c-.41.24-.788.54-1.148.94-.27.32-.421.639-.461.949a1.24 1.24 0 0 0 .05.541l.02.01a13.722 13.722 0 0 0 1.2 2.6 15.383 15.383 0 0 0 2.32 3.171l.03.04.04.03.03.03.03.03a15.603 15.603 0 0 0 3.18 2.33c1.32.72 2.122 1.06 2.602 1.2v.01c.14.04.268.06.398.06a1.84 1.84 0 0 0 1.102-.472c.39-.35.7-.738.93-1.148v-.01c.23-.43.15-.841-.18-1.121a13.632 13.632 0 0 0-2.15-1.54c-.51-.28-1.03-.11-1.24.17l-.45.569c-.23.28-.65.24-.65.24l-.012.01c-3.12-.8-3.95-3.959-3.95-3.959s-.04-.43.25-.65l.56-.45c.27-.22.46-.74.17-1.25a13.522 13.522 0 0 0-1.54-2.15.843.843 0 0 0-.504-.3zm4.473.89a.3.3 0 0 0 .002.6 3.78 3.78 0 0 1 2.65 1.15 3.5 3.5 0 0 1 .9 2.57.3.3 0 0 0 .3.299l.01.012a.3.3 0 0 0 .3-.301c.03-1.19-.34-2.19-1.07-2.99-.73-.8-1.75-1.25-3.05-1.34a.3.3 0 0 0-.042 0zm.49 1.619a.305.305 0 0 0-.018.611c.99.05 1.47.55 1.53 1.58a.3.3 0 0 0 .3.29h.01a.3.3 0 0 0 .29-.32c-.07-1.34-.8-2.091-2.1-2.161a.305.305 0 0 0-.012 0z"></path>
+                            </svg>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.youtube.com/channel/UCuJCYvI-WOJ9awI3Gxo_sMw"
+                            target="_blank"
+                            class="youtube-icon"
+                          >
+                            <svg
+                              stroke="currentColor"
+                              fill="currentColor"
+                              stroke-width="0"
+                              viewBox="0 0 24 24"
+                              height="1em"
+                              width="1em"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <g>
+                                <path fill="none" d="M0 0h24v24H0z"></path>
+                                <path d="M21.543 6.498C22 8.28 22 12 22 12s0 3.72-.457 5.502c-.254.985-.997 1.76-1.938 2.022C17.896 20 12 20 12 20s-5.893 0-7.605-.476c-.945-.266-1.687-1.04-1.938-2.022C2 15.72 2 12 2 12s0-3.72.457-5.502c.254-.985.997-1.76 1.938-2.022C6.107 4 12 4 12 4s5.896 0 7.605.476c.945.266 1.687 1.04 1.938 2.022zM10 15.5l6-3.5-6-3.5v7z"></path>
+                              </g>
+                            </svg>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.pinterest.com/nabilbank/_saved/"
+                            target="_blank"
+                            class="pinterest-icon"
+                          >
+                            <svg
+                              stroke="currentColor"
+                              fill="currentColor"
+                              stroke-width="0"
+                              viewBox="0 0 24 24"
+                              height="1em"
+                              width="1em"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <g>
+                                <path fill="none" d="M0 0h24v24H0z"></path>
+                                <path d="M13.37 2.094A10.003 10.003 0 0 0 8.002 21.17a7.757 7.757 0 0 1 .163-2.293c.185-.839 1.296-5.463 1.296-5.463a3.739 3.739 0 0 1-.324-1.577c0-1.485.857-2.593 1.923-2.593a1.334 1.334 0 0 1 1.342 1.508c0 .9-.578 2.262-.88 3.54a1.544 1.544 0 0 0 1.575 1.923c1.898 0 3.17-2.431 3.17-5.301 0-2.2-1.457-3.848-4.143-3.848a4.746 4.746 0 0 0-4.93 4.794 2.96 2.96 0 0 0 .648 1.97.48.48 0 0 1 .162.554c-.046.184-.162.623-.208.784a.354.354 0 0 1-.51.254c-1.384-.554-2.036-2.077-2.036-3.816 0-2.847 2.384-6.255 7.154-6.255 3.796 0 6.32 2.777 6.32 5.747 0 3.909-2.177 6.848-5.394 6.848a2.861 2.861 0 0 1-2.454-1.246s-.578 2.316-.692 2.754a8.026 8.026 0 0 1-1.019 2.131c.923.28 1.882.42 2.846.416a9.988 9.988 0 0 0 9.996-10.003 10.002 10.002 0 0 0-8.635-9.903z"></path>
+                              </g>
+                            </svg>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
                   </li>
                 </ul>
                 <a href="" class="btn red-btn">
@@ -62,7 +222,7 @@ const Footer = () => {
               </div>
               <div class="col-md-6 col-lg-2 page-more-info">
                 <div class="footer-title">
-                  <h4>Page links</h4>
+                  <h4>About Us</h4>
                 </div>
                 <ul>
                   <li>
@@ -85,7 +245,7 @@ const Footer = () => {
 
               <div class="col-md-6 col-lg-3 page-more-info">
                 <div class="footer-title">
-                  <h4>More Info</h4>
+                  <h4>Corporate Social Responsibility</h4>
                 </div>
                 <ul>
                   <li>
@@ -102,7 +262,52 @@ const Footer = () => {
                   </li>
                 </ul>
               </div>
-              <div class="col-md-6 col-lg-4 open-hours">
+              <div class="col-md-6 col-lg-2 page-more-info">
+                <div class="footer-title">
+                  <h4>Support</h4>
+                </div>
+                <ul>
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Testimonial</a>
+                  </li>
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-md-6 col-lg-2 page-more-info">
+                <div class="footer-title">
+                  <h4>Important links</h4>
+                </div>
+                <ul>
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Testimonial</a>
+                  </li>
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact</a>
+                  </li>
+                </ul>
+              </div>
+
+              {/* <div class="col-md-6 col-lg-4 open-hours">
                 <div class="footer-title">
                   <h4>Open hours</h4>
                   <ul class="footer-social">
@@ -174,10 +379,12 @@ const Footer = () => {
                     </tbody>
                   </table>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
+
           <hr />
+
           <div class="footer-bottom">
             <div class="row">
               <div class="col-sm-4">
