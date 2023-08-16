85e22d4ad2650052425e7b4c388fca45ec807391
vi-sha-kha <vishakha.ch.5281@gmail.com>
2023-08-16 16:53:34 +0545

    MODIFIED responsiveness

--- src/components/section/Promo.js
+++ src/components/section/Promo.js
@@ -4,54 +4,6 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 const Promo = () => {
   return (
     <>
-      {/* <div class="container">
-        <div class="row align-items-center pb-3 pb-md-0">
-          <div class="col-md-7 order-1 order-md-0">
-            <h2 class="font-weight-bold text-danger mb-3">
-              Want advice{" "}
-              <span class="text-success font-weight-bold ">
-                {" "}
-                or know what you are looking for?
-              </span>{" "}
-            </h2>
-            <h5 class=" text-d-gray mb-3">
-              Answer a few questions and we will help you plan your finances.
-            </h5>
-            <div class="d-flex flex-column flex-sm-row justify-content-center  ">
-              <div className="row g-2">
-                <div className="col">
-                  <Button variant="success">
-                    Schedule an Appointment{" "}
-                    <i
-                      style={{ marginLeft: "10px;" }}
-                      class="fa fa-angle-double-right"
-                      aria-hidden="true"
-                    ></i>
-                  </Button>
-                </div>
-                <div className="col">
-                  <Button variant="outline-success">
-                    View FAQs{" "}
-                    <i
-                      style={{ marginLeft: "10px;" }}
-                      class="fa fa-angle-double-right"
-                      aria-hidden="true"
-                    ></i>
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-5 order-0 order-md-1">
-            <img
-              alt=""
-              src="https://siteadmin.nabilbank.com/assets/backend/uploads/home/Individual/Want &amp; Advice-06.png"
-              class="img-fluid"
-            />
-          </div>
-        </div>
-      </div> */}
-
       <Container>
         <Row className="align-items-center">
           <Col>
--- src/components/section/Promo2.js
+++ src/components/section/Promo2.js
@@ -1,94 +1,89 @@
 import React from "react";
-import { Row, Col, Button } from "react-bootstrap";
+import { Row, Col, Button, Container } from "react-bootstrap";
 
 const Promo2 = () => {
   return (
     <>
-      <div className="container">
+      <Container>
         <Row className="align-items-center">
-          <Col md={6} className="position-relative">
-            <div className="position-relative m-3">
-              <figure className="m-0">
-                <img
-                  alt="OOPS"
-                  src='"https://siteadmin.nabilbank.com/assets/backend/uploads/home/Individual/nBank%20Banner.png"'
-                  class="img-fluid"
-                />
-                <img
-                  alt=""
-                  src="https://siteadmin.nabilbank.com/assets/backend/uploads/home/Individual/Want &amp; Advice-06.png"
-                  class="img-fluid"
-                />
-              </figure>
-            </div>
+          <Col>
+            <img
+              src="https://siteadmin.nabilbank.com/assets/backend/uploads/home/Individual/Want &amp; Advice-06.png"
+              class="img-fluid"
+              alt=""
+            />
           </Col>
-          <Col md={6}>
-            <h2 className="font-weight-bold text-danger mb-4">
-              Bank from{" "}
-              <span className="text-success font-weight-bold">everywhere</span>
-            </h2>
-            <h5 className="font-weight-500 text-d-gray mb-4">
-              Change the way you bank, with nBank.
-            </h5>
-            <Row className="app-service-grid">
-              <Col className="service-block active">
-                <img
-                  alt=""
-                  src="https://siteadmin.nabilbank.com/assets/backend/uploads/home/Individual/Vector.png"
-                />
-                Mobile
-              </Col>
-              <Col className="service-block ">
-                <img
-                  alt=""
-                  src="https://siteadmin.nabilbank.com/assets/backend/uploads/home/Individual/Vector-1.png"
-                />
-                Internet
-              </Col>
-              <Col class="service-block ">
-                <img
-                  alt=""
-                  src="https://siteadmin.nabilbank.com/assets/backend/uploads/Banner Menu/Bank.png"
-                />
-                Branches
-              </Col>
-              <Col class="service-block ">
-                <img
-                  alt=""
-                  src="https://siteadmin.nabilbank.com/assets/backend/uploads/home/Individual/Group.png"
-                />
-                ATM
-              </Col>
-              {/* Other service-block components */}
-              <div className="service_icon-arrow"></div>
-            </Row>
-            <div className="d-flex mx-5 flex-column flex-sm-row flex-wrap justify-content-center">
-              <Row className="g-2">
-                <Col>
-                  <Button variant="success">
-                    Send to Mobile Number{" "}
-                    <i
-                      style={{ marginLeft: "10px;" }}
-                      class="fa fa-angle-double-right"
-                      aria-hidden="true"
-                    ></i>
-                  </Button>
+          <Col>
+            <div class="pr-0 pr-sm-5">
+              <h2 className="font-weight-bold text-danger mb-4">
+                Bank from{" "}
+                <span className="text-success font-weight-bold">
+                  everywhere
+                </span>
+              </h2>
+              <h5 className="font-weight-500 text-d-gray mb-4">
+                Change the way you bank, with nBank.
+              </h5>
+              <Row className="app-service-grid">
+                <Col className="service-block active">
+                  <img
+                    alt=""
+                    src="https://siteadmin.nabilbank.com/assets/backend/uploads/home/Individual/Vector.png"
+                  />
+                  Mobile
                 </Col>
-                <Col>
-                  <Button variant="outline-success">
-                    More Info{" "}
-                    <i
-                      style={{ marginLeft: "10px;" }}
-                      class="fa fa-angle-double-right"
-                      aria-hidden="true"
-                    ></i>
-                  </Button>
+                <Col className="service-block ">
+                  <img
+                    alt=""
+                    src="https://siteadmin.nabilbank.com/assets/backend/uploads/home/Individual/Vector-1.png"
+                  />
+                  Internet
                 </Col>
+                <Col class="service-block ">
+                  <img
+                    alt=""
+                    src="https://siteadmin.nabilbank.com/assets/backend/uploads/Banner Menu/Bank.png"
+                  />
+                  Branches
+                </Col>
+                <Col class="service-block ">
+                  <img
+                    alt=""
+                    src="https://siteadmin.nabilbank.com/assets/backend/uploads/home/Individual/Group.png"
+                  />
+                  ATM
+                </Col>
+                {/* Other service-block components */}
+                <div className="service_icon-arrow"></div>
               </Row>
+              <div className="d-flex mx-5 flex-column flex-sm-row flex-wrap justify-content-center">
+                <Row className="g-2">
+                  <Col>
+                    <Button variant="success">
+                      Send to Mobile Number{" "}
+                      <i
+                        style={{ marginLeft: "10px;" }}
+                        class="fa fa-angle-double-right"
+                        aria-hidden="true"
+                      ></i>
+                    </Button>
+                  </Col>
+                  <Col>
+                    <Button variant="outline-success">
+                      More Info{" "}
+                      <i
+                        style={{ marginLeft: "10px;" }}
+                        class="fa fa-angle-double-right"
+                        aria-hidden="true"
+                      ></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </div>
             </div>
           </Col>
         </Row>
-      </div>
+      </Container>
     </>
   );
 };
