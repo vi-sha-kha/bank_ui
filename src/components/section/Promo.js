3585948196de6e0afe98ac3f41d035243fa402f6
vi-sha-kha <vishakha.ch.5281@gmail.com>
2023-08-16 16:39:17 +0545

    modified responsiveness

--- src/components/section/Promo.js
+++ src/components/section/Promo.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 const Promo = () => {
   return (
     <>
-      <div class="container">
+      {/* <div class="container">
         <div class="row align-items-center pb-3 pb-md-0">
           <div class="col-md-7 order-1 order-md-0">
             <h2 class="font-weight-bold text-danger mb-3">
@@ -50,7 +50,56 @@ const Promo = () => {
             />
           </div>
         </div>
-      </div>
+      </div> */}
+
+      <Container>
+        <Row className="align-items-center">
+          <Col>
+            <div class="pr-0 pr-sm-5">
+              {/* <h4 class="font-weight-bold text-success">Our Stories</h4> */}
+              <h2 class="text-danger">
+                Want advice{" "}
+                <span class="font-weight-bold text-success font-weight-bold ">
+                  {" "}
+                  or know what you are looking for?
+                </span>
+              </h2>
+              <h5 class="font-weight-bold text-purple mb-5"></h5>
+              <div class="d-flex flex-column flex-sm-row justify-content-center  ">
+                <div className="row g-2">
+                  <div className="col">
+                    <Button variant="success">
+                      Schedule an Appointment{" "}
+                      <i
+                        style={{ marginLeft: "10px;" }}
+                        class="fa fa-angle-double-right"
+                        aria-hidden="true"
+                      ></i>
+                    </Button>
+                  </div>
+                  <div className="col">
+                    <Button variant="outline-success">
+                      View FAQs{" "}
+                      <i
+                        style={{ marginLeft: "10px;" }}
+                        class="fa fa-angle-double-right"
+                        aria-hidden="true"
+                      ></i>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <img
+              src="https://siteadmin.nabilbank.com/assets/backend/uploads/home/Individual/Want &amp; Advice-06.png"
+              class="img-fluid"
+              alt=""
+            />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
