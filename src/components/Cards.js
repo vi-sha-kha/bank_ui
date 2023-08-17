7821062261dff4c806d5915e311a37f6ce1ce60d
vi-sha-kha <vishakha.ch.5281@gmail.com>
2023-08-17 17:07:37 +0545

    modified breakpoints

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,10 +8,10 @@ const Cards = ({ img, title }) => {
       <div class="card cardcc mb-3 shadow-lg p-3 mb-5 bg-white rounded mt-3 ">
         <div className="category-wrapper-item">
           <div class="row g-0">
-            <div class="col-md-4 col-sm-4 col-xs-4 ">
+            <div class="col-md-6 col-sm-12 col-xs-12 ">
               <img src={img} class="img-fluid rounded-start" alt="...TO ADD" />
             </div>
-            <div class="col-md-8 col-sm-8 col-xs-8 ">
+            <div class="col-md-6 col-sm-12 col-xs-12 ">
               <div class="card-body">
                 <h5 class="card-title">{title}</h5>
               </div>
